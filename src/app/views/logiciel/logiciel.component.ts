0b19100914e8b8a31fb75cbe854e5fb0c8aee32b
GesKaD <karl.delourmel@gmx.fr>
2017-04-30 15:34:56 +0200

    contenu

--- src/app/views/logiciel/logiciel.component.ts
+++ src/app/views/logiciel/logiciel.component.ts
@@ -11,44 +11,32 @@ import { TeacherService } from '../../services/logiciel.service'
 export class logicielComponent { 
 
 	teachers: any = [
-		{ titre: "CLYO ",
-			texte: "C'est le plus mieux mais le plus chere",
-			job: "CS FIRST",
-			company: "Sloth Factory",
-			localisation: "le Pub du coin",
-			tel: "777.777.777",
-			adresse:"14 Rue Saint-Hélier",
-			ville: "35000 Rennes",
+		{ titre: "CS FIRST ",
+			descript: "Concu pour les commerces en début d'activité ou désirant une grande simplicité d'utilisation",
+			compo: "CS FISRT comprend l'encaissement, les statistiques et la programmation",
+			conclu: "Commencer simple pour pouvoir évoluer vers les licences plus completes",
+			job: "LOGICIEL DE CAISSE VERSION FIRST"
 			pic: "../../assets/img/landing/logo/clyofirst.jpg"},
 
-		{ first: "CLYO ",
-			last: "DELAUNE",
-			job: "CS STANDARD",
-			company: "Error Factory",
-			localisation: "Le Tregor...",
-			tel: "666.666.666",
-			adresse: "1 Rue de la perdition numérique",
-			ville: "22300 Lannion",
+		{ titre: "CS STD",
+			descript: "Pour les clients plus exigent ou pour plusieurs points d'encaissements",
+			compo: "CS STD comprend l'encaissement, les statistiques et la programmation",
+			conclu: "Aborder les fortes afluances avec cérénité",
+			job: "LOGICIEL DE CAISSE VERSION STD"
 			pic: "../../assets/img/landing/logo/clyostd.jpg"},
 
-		{ first: "CLYO ",
-			last: "SENIOR",
-			job: "CS PME",
-			company: "himself",
-			localisation: "his bed",
-			tel : "no tel",
-			adresse: "2 rue du lit",
-			ville: "In your Ass",
+		{ titre: "CS PME ",
+			descript: "Soyer plus proche de vos clients en gardant le contact",
+			compo: "CS PME comprend l'encaissement, les statistiques, la programmation et la relation client",
+			conclu: "Developper votre clientel",
+			job: "LOGICIEL DE CAISSE VERSION PME"
 			pic: "../../assets/img/landing/logo/clyopme.jpg"},
 
-		{ first: "CLYO ",
-			last: "DELOURMEL",
-			job: "PRO",
-			company: "Burelec",
-			localisation: "le Pub du coin",
-			tel: "777.777.777",
-			adresse:"14 Rue Saint-Hélier",
-			ville: "35000 Rennes",
+		{ titre: "CS PRO ",
+			descript: "Maitriser votre marchandise",
+			compo: "CS PRO comprend l'encaissement, les statistiques, la programmation, la relation client et la gestion de stock",
+			conclu: "Les grands projets meritent le bon logiciel",
+			job: "LOGICIEL DE CAISSE VERSION PRO"
 			pic: "../../assets/img/landing/logo/clyopro.jpg"},
 	];
 
