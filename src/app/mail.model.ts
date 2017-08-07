fb622dfb7eb798f65c8396a13a5c797bfbc86211
GesKaD <karl.delourmel@gmx.fr>
2017-08-07 12:33:05 +0200

    form work

---
+++ src/app/mail.model.ts
@@ -0,0 +1,12 @@
+export class Mail {
+	constructor (
+		public RaisSoc: string,
+		public Nom: string,
+		public Prenom: string,
+		public TypeCom: string,
+		public Tel: string,
+		public Email: string,
+		public CPVille: string,
+		public Texte: string,
+	) { }
+}
